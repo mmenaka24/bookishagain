77d7facb3a933eba5a632cbd6febe49fe0c7e2a7
Menaka Maruthainar <menaka.maruthainar@softwire.com>
2025-05-02 15:58:16 +0100

    fix: wrong type in table setup
    changes type of book_isbn in book_author junction table to be string to match the books table

--- bookish/models/junction_tables.py
+++ bookish/models/junction_tables.py
@@ -4,5 +4,5 @@ from bookish.app import db
 class BookAuthor(db.Model):
     __tablename__ = "book_author"
 
-    book_isbn = db.Column(db.Integer, db.ForeignKey("Books.isbn"), primary_key=True)
+    book_isbn = db.Column(db.String(13), db.ForeignKey("Books.isbn"), primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey("Authors.id", primary_key=True))
